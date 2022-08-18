e1c829fdc916b12da6f766ffaf1e29ebbfac5141
Zhichao Liu <zhichao.liu@snowflake.com>
2022-08-18 04:38:33 +0000

    add SnowflakeFile interface and tests

---
+++ tests/unit/test_files.py
@@ -0,0 +1,30 @@
+#
+# Copyright (c) 2012-2022 Snowflake Computing Inc. All rights reserved.
+#
+import pytest
+
+from snowflake.snowpark.files import SnowflakeFile
+
+
+def test_create_snowflakefile():
+    snowflake_file = SnowflakeFile.open("test_file_location")
+
+    assert snowflake_file._file_location == "test_file_location"
+    assert snowflake_file._mode == "r"
+
+
+def test_snowflake_file_attribute():
+    snowflake_file = SnowflakeFile.open("test_file_location")
+
+    assert snowflake_file.buffer is None
+    assert snowflake_file.closed is None
+    assert snowflake_file.encoding is None
+    assert snowflake_file.errors is None
+
+
+def test_snowflake_file_method():
+    snowflake_file = SnowflakeFile.open("test_file_location")
+
+    with pytest.raises(NotImplementedError):
+        snowflake_file.close()
+    # TBD: maybe try every single methods here?
